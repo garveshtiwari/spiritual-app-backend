0d95fbc606b7fad449282220c9ab7ec98b20b9b5
tiwari-garvesh <garveshtiwari1999@gmail.com>
2026-07-28 18:03:35 +0530

    initialized springboot project

---
+++ src/main/java/com/garveshtiwari/spiritual_app_backend/SpiritualAppBackendApplication.java
@@ -0,0 +1,14 @@
+package com.garveshtiwari.spiritual_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiritualAppBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpiritualAppBackendApplication.class, args);
+	}
+
+}
