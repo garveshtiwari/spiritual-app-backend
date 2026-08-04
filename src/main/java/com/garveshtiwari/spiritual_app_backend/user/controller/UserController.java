2aae9fff06fb0328697d082aee6ca09039554905
tiwari-garvesh <garveshtiwari1999@gmail.com>
2026-08-05 00:27:20 +0530

    added profile API

---
+++ src/main/java/com/garveshtiwari/spiritual_app_backend/user/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.garveshtiwari.spiritual_app_backend.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    @GetMapping("/profile")
+    public ResponseEntity<String> getProfile(
+            Authentication authentication
+    ) {
+
+        String email = authentication.getName();
+
+        return ResponseEntity.ok(
+                "Authenticated user: " + email
+        );
+    }
+}
