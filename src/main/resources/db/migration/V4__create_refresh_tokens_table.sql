21a12659682e73b29f7abb1205fc0ca7faa398d7
tiwari-garvesh <garveshtiwari1999@gmail.com>
2026-08-05 02:08:15 +0530

    Flyway changes added refresh token

---
+++ src/main/resources/db/migration/V4__create_refresh_tokens_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE refresh_tokens
+(
+    id BIGSERIAL PRIMARY KEY,
+
+    token VARCHAR(500) NOT NULL UNIQUE,
+
+    user_id BIGINT NOT NULL,
+
+    expiry_date TIMESTAMP NOT NULL,
+
+    CONSTRAINT fk_refresh_token_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
