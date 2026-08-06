c601b903c63be3ae25ce05cc8a15b0658a849af1
tiwari-garvesh <garveshtiwari1999@gmail.com>
2026-08-07 01:04:07 +0530

    made necessary changes to book repo, app theme enum and sampradaya enum

--- src/main/java/com/garveshtiwari/spiritual_app_backend/book/repository/BookRepository.java
+++ src/main/java/com/garveshtiwari/spiritual_app_backend/book/repository/BookRepository.java
@@ -1,11 +1,14 @@
 package com.garveshtiwari.spiritual_app_backend.book.repository;
 
 import com.garveshtiwari.spiritual_app_backend.book.entity.Book;
+import com.garveshtiwari.spiritual_app_backend.common.enums.AppLanguage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository
+        extends JpaRepository<Book, Long> {
 
     Optional<Book> findBySlug(
             String slug
@@ -17,5 +20,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     boolean existsBySlug(
             String slug
+    );
+
+    List<Book> findByLanguage(
+            AppLanguage language
     );
 }
