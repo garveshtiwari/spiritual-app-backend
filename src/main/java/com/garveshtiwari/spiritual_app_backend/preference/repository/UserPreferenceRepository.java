9d3aa411f8ebcb5a4a2a737f1d89a14981c60a9c
tiwari-garvesh <garveshtiwari1999@gmail.com>
2026-08-07 01:15:57 +0530

    added user preference and user preferred book repo

---
+++ src/main/java/com/garveshtiwari/spiritual_app_backend/preference/repository/UserPreferenceRepository.java
@@ -0,0 +1,18 @@
+package com.garveshtiwari.spiritual_app_backend.preference.repository;
+
+import com.garveshtiwari.spiritual_app_backend.preference.entity.UserPreference;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserPreferenceRepository
+        extends JpaRepository<UserPreference, Long> {
+
+    Optional<UserPreference> findByUserId(
+            Long userId
+    );
+
+    boolean existsByUserId(
+            Long userId
+    );
+}
---
+++ src/main/java/com/garveshtiwari/spiritual_app_backend/preference/repository/UserPreferredBookRepository.java
@@ -0,0 +1,30 @@
+package com.garveshtiwari.spiritual_app_backend.preference.repository;
+
+import com.garveshtiwari.spiritual_app_backend.preference.entity.UserPreferredBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserPreferredBookRepository
+        extends JpaRepository<UserPreferredBook, Long> {
+
+    List<UserPreferredBook> findByUserPreferenceId(
+            Long userPreferenceId
+    );
+
+    Optional<UserPreferredBook>
+    findByUserPreferenceIdAndBookId(
+            Long userPreferenceId,
+            Long bookId
+    );
+
+    boolean existsByUserPreferenceIdAndBookId(
+            Long userPreferenceId,
+            Long bookId
+    );
+
+    void deleteByUserPreferenceId(
+            Long userPreferenceId
+    );
+}
