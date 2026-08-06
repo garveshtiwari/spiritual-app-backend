06b89906c9e71a67a4d0a002ff08a292b6bbf703
tiwari-garvesh <garveshtiwari1999@gmail.com>
2026-08-07 05:07:53 +0530

    added enum to determine knowledge source

---
+++ src/main/java/com/garveshtiwari/spiritual_app_backend/common/enums/KnowledgeSource.java
@@ -0,0 +1,16 @@
+package com.garveshtiwari.spiritual_app_backend
+        .common.enums;
+
+public enum KnowledgeSource {
+
+    VERSE,
+    CHAPTER,
+    COMMENTARY,
+    TRANSLATION,
+    SUMMARY,
+    QUOTE,
+    FAQ,
+    JOURNAL_TEMPLATE,
+    ARTICLE,
+    SCRIPTURE
+}
