7387c01a8f3ad9413afcfee6bb76ff600a345695
tiwari-garvesh <garveshtiwari1999@gmail.com>
2026-08-07 01:55:14 +0530

    added journal and history repo

--- src/main/java/com/garveshtiwari/spiritual_app_backend/history/repository/ReadingHistoryRepository.java
+++ src/main/java/com/garveshtiwari/spiritual_app_backend/history/repository/ReadingHistoryRepository.java
@@ -4,12 +4,18 @@ import com.garveshtiwari.spiritual_app_backend.history.entity.ReadingHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReadingHistoryRepository
         extends JpaRepository<ReadingHistory, Long> {
 
     List<ReadingHistory>
     findByUserIdOrderByOpenedAtDesc(
+            Long userId
+    );
+
+    Optional<ReadingHistory> findByIdAndUserId(
+            Long id,
             Long userId
     );
 }
--- src/main/java/com/garveshtiwari/spiritual_app_backend/journal/repository/JournalEntryRepository.java
+++ src/main/java/com/garveshtiwari/spiritual_app_backend/journal/repository/JournalEntryRepository.java
@@ -4,12 +4,18 @@ import com.garveshtiwari.spiritual_app_backend.journal.entity.JournalEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JournalEntryRepository
         extends JpaRepository<JournalEntry, Long> {
 
     List<JournalEntry>
     findByUserIdOrderByCreatedAtDesc(
+            Long userId
+    );
+
+    Optional<JournalEntry> findByIdAndUserId(
+            Long id,
             Long userId
     );
 }
