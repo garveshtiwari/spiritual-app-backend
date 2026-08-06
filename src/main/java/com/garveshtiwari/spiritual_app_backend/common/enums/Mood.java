c02817d98cbcc4d948a717d80b2c67c1d5f541a6
tiwari-garvesh <garveshtiwari1999@gmail.com>
2026-08-07 01:05:09 +0530

    added mood enum for user mood

---
+++ src/main/java/com/garveshtiwari/spiritual_app_backend/common/enums/Mood.java
@@ -0,0 +1,15 @@
+package com.garveshtiwari.spiritual_app_backend.common.enums;
+
+public enum Mood {
+
+    SAD,
+    ANXIOUS,
+    ANGRY,
+    CONFUSED,
+    LONELY,
+    STRESSED,
+    FEARFUL,
+    GRATEFUL,
+    MOTIVATED,
+    PEACEFUL
+}
